5bb0b3094850fbaca0d3bb302f90f0507f63174d
Mrbroggle <40074860+Mrbroggle@users.noreply.github.com>
2022-04-12 16:01:14 +1000

    Update keymap.c

--- keyboards/contra/keymaps/mrbroggle/keymap.c
+++ keyboards/contra/keymaps/mrbroggle/keymap.c
@@ -106,8 +106,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------------------------------------------------'
    */
   [_LOWER] = LAYOUT_planck_mit(
-    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
     KC_LSFT, KC_INS,  KC_HOME, KC_DEL,  KC_END,  KC_PGUP, KC_PGDN, KC_PSCR, KC_SLCK, KC_PAUS, KC_GRV,  KC_CAPS,
     KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  KC_SPC,  KC_RALT, KC_RGUI, KC_APP,  KC_RCTL
     
